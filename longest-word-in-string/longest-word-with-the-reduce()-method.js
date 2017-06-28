98022d76c40a4ec88461a0eca8ebb633661c9eb6
sinhadroid <107sinha@gmail.com>
2017-06-28 22:57:32 +0530

    longest word in a string

---
+++ longest-word-in-string/longest-word-with-the-reduce()-method.js
@@ -0,0 +1,7 @@
+function findLongestWord(str) {
+  var longestWord = str.split(' ').reduce(function(longest, currentWord) {
+    return currentWord.length > longest.length ? currentWord : longest;
+  }, "");
+  return longestWord.length;
+}
+findLongestWord("The quick brown fox jumped over the lazy dog");
