c5c36f2e2f65b4bb2d87282fd34a8b29dc8419f8
sinhaDroid <107sinha@gmail.com>
2018-07-28 17:52:31 +0530

    title case a sentence two advance solution

---
+++ title-case-a-sentence/advanced-2-solution.js
@@ -0,0 +1,18 @@
+// Title Case a Sentence
+// "I'm a little tea pot"
+// Advanced Solution
+
+function titleCase(str) {
+    str = str.split(' ');
+    for (let index = 0; index < str.length; index++) {
+        str[index] = str[index].toLowerCase().split('');
+        str[index][0] = str[index][0].toUpperCase();
+        str[index] = str[index].join(''); 
+    }
+    return str.join(' ');
+}
+
+console.log(titleCase("I'm a little tea pot"));
+
+// Solution
+// I'm A Little Tea Pot
